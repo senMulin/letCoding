8f5e34dcff9eadc407e888afd89b8516224c7a56
wangsen <wangsen@ilongyuan.com.cn>
2018-08-13 23:54:14 +0800

    leetcoding 算法练习,合并二叉树

---
+++ src/main/java/com/mulin/letCoding/tree/TreeNode.java
@@ -0,0 +1,15 @@
+package com.mulin.letCoding.tree;
+
+/**
+ * Created by mulin on 2018/8/13.
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+}
