0657cba89847fe3e5bc3f8d528da11762b046306
cdwangsen <wangsen@ilongyuan.com.cn>
2018-09-27 18:06:34 +0800

    leetcode 完全平方数 [https://leetcode-cn.com/problems/perfect-squares]

--- src/main/java/com/mulin/letcoding/dynamicprograming/middle/PerfectSquares.java
+++ src/main/java/com/mulin/letcoding/dynamicprograming/middle/PerfectSquares.java
@@ -1,5 +1,7 @@
 package com.mulin.letcoding.dynamicprograming.middle;
 
+import java.util.*;
+
 /**
  * Created by
  *
@@ -21,8 +23,51 @@ package com.mulin.letcoding.dynamicprograming.middle;
  */
 public class PerfectSquares {
 
+    List<Integer> list = new ArrayList();
+
+    Map<Integer, Integer> map = new HashMap<>();
+
     public int numSquares(int n) {
 
-        return n;
+        int num = 0;
+        while ((num + 1) * (num + 1) <= n) {
+
+            num++;
+            list.add(num * num);
+        }
+
+        return findMin(n);
+    }
+
+    public int findMin(int n) {
+
+        int sum = 1;
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n < 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for (Integer i : list) {
+
+           Integer result = map.get(n - i);
+           if (null == result) {
+               result = findMin(n - i);
+           }
+           map.put(n - i, result);
+           min = Math.min(min, result);
+        }
+
+        return sum + min;
+    }
+
+    public static void main(String[] args) {
+        PerfectSquares p = new PerfectSquares();
+
+        System.out.println(p.numSquares(88));
     }
 }
